168a8ac395dc575012f5a888fc3eebc09e58ecda
SiebeVerhaegen <r1002687@student.thomasmore.be>
2026-03-24 20:56:56 +0100

    Add dynamic URL for data fetching.

--- src/app/about/TopTenList.tsx
+++ src/app/about/TopTenList.tsx
@@ -1,11 +1,13 @@
 import { IBoardGame } from "@/interfaces/BoardGame"
 import React from "react"
 import TopTenListItem from "./TopTenListItem"
+import { getBaseUrl } from "@/lib/utils"
 
 async function TopTenList() {
-  const topTenResponse = await fetch("http://localhost:3000/api/topTenList")
+  const baseUrl = getBaseUrl()
+  const topTenResponse = await fetch(`${baseUrl}/api/topTenList`)
   const topTenData = await topTenResponse.json()
-  const collectionResponse = await fetch("http://localhost:3000/api/collection?ids=" + topTenData.items.join(","))
+  const collectionResponse = await fetch(`${baseUrl}/api/collection?ids=${topTenData.items.join(",")}`)
 
   const collectionData: IBoardGame[] = await collectionResponse.json()
 
